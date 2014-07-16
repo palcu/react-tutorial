bf81d51260f60fb9bf3f59274555e4b79c770923
Alex Palcuie <alex.palcuie@hootsuite.com>
2014-07-16 12:38:04 +0300

    Add first version of tutorial

---
+++ tutorial.js
@@ -0,0 +1,12 @@
+var CommentBox = React.createClass({
+  render: function({
+    return (
+      <div className="commentBox">
+        Hello world! I am a CommentBox.
+      </div>
+    );
+  });
+});
+
+React.renderComponent(<CommentBox />,
+                      document.getElementById('content'));
